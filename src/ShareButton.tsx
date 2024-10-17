831b4febc88c93adedc4b8813ee576d5373ab90f
Sam Behrens <sbehrens1414@gmail.com>
2024-10-17 16:58:13 -0600

    Add try catches to use of navigator

--- src/ShareButton.tsx
+++ src/ShareButton.tsx
@@ -10,21 +10,45 @@ type ShareButtonProps = {
 
 export function ShareButton(props: ShareButtonProps) {
   let shareApiAvailable = useMemo(() => {
-    return (
-      "canShare" in navigator &&
-      "share" in navigator &&
-      navigator.canShare(props)
-    );
+    let available = false;
+    try {
+      available =
+        "canShare" in navigator &&
+        "share" in navigator &&
+        navigator.canShare(props);
+    } catch (err) {
+      available = false;
+    }
+    return available;
   }, [props.text]);
+
   console.log(shareApiAvailable);
   return (
     <>
       {shareApiAvailable && (
-        <button className="link inline-flex items-center justify-center gap-xs" onClick={() => navigator.share(props)}>
+        <button
+          className="link inline-flex items-center justify-center gap-xs"
+          onClick={() => {
+            try {
+              navigator.share(props);
+            } catch (err) {
+              // do nothing
+            }
+          }}
+        >
           <Icon icon={share} /> Share
         </button>
       )}
-      <button className="link inline-flex items-center justify-center gap-xs" onClick={() => navigator.clipboard.writeText(props.url)}>
+      <button
+        className="link inline-flex items-center justify-center gap-xs"
+        onClick={() => {
+          try {
+            navigator.clipboard.writeText(props.url);
+          } catch (err) {
+            // do nothing
+          }
+        }}
+      >
         <Icon icon={clipboard} /> Copy link
       </button>
     </>
